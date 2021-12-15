37ba4e5bcaa9b4661aad07633da5b4d12aa3d686
姚一鸣 <yaoyiming123@live.com>
2021-12-15 21:44:53 +0800

    【英雄详情】英雄详情页面添加一个头像

--- OpenDota+/View/DotaHeroInfoView.swift
+++ OpenDota+/View/DotaHeroInfoView.swift
@@ -5,6 +5,7 @@
 //  Created by 姚一鸣 on 2021/12/14.
 //
 
+import Kingfisher
 import SwiftUI
 
 struct DotaHeroInfoView: View {
@@ -13,6 +14,13 @@ struct DotaHeroInfoView: View {
     var dotaHero: DotaHeroModel
 
     var body: some View {
-        Text(dotaHero.localized_name ?? "Unknown")
+        KFImage(URL(string: "https://cdn.cloudflare.steamstatic.com" + dotaHero.img)!)
+            .placeholder { Image("item_placeholder").resizable().scaledToFill().frame(width: 128, height: 72, alignment: .center) }
+            .resizable()
+            .frame(width: 128, height: 72, alignment: .leading)
+            .scaledToFill()
+            .cornerRadius(4)
+            .shadow(radius: 10)
+        Text(dotaHero.localized_name ?? "Unknown").bold()
     }
 }
